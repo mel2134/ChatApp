3c647d51bc8c6b5359b79e68c3baca29199195ef
mel2134 <158561962+mel2134@users.noreply.github.com>
2025-03-21 11:01:04 +0200

    console

---
+++ ChatApp/ChatClientConsole/Net/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatClientConsole.Net
+{
+    class User
+    {
+        public string Username { get; set; }
+        public string UID { get; set; }
+    }
+}
---
+++ ChatApp/ChatClientConsole/Program.cs
@@ -0,0 +1,37 @@
+﻿using ChatClient.Net;
+
+namespace ChatClientConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Server server = new Server();
+            while (true)
+            {
+                if (!server._connected)
+                {
+                    try
+                    {
+                        Console.Clear();
+                        Console.Write("Host: ");
+                        string host = Console.ReadLine();
+                        Console.Write("Port: ");
+                        int port = int.Parse(Console.ReadLine());
+                        Console.Write("Username: ");
+                        string username = Console.ReadLine();
+                        Console.Title = $"({username}) !exit to disconnect";
+                        server.ConnectToServer(username, host, port);
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Invalid data has been inserted");
+                        Thread.Sleep(2000);
+                        Console.Clear();
+                    }
+                }
+                Thread.Sleep(3000);
+            }
+        }
+    }
+}
